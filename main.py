f3abe8adc9a0299570480159150da4149750a986
Gabriel-Jacquinot <gabejacquinot0@gmail.com>
2025-09-26 20:01:44 +0100

    Font

--- main.py
+++ main.py
@@ -4,20 +4,21 @@ from menuMain import Menu
 
 pygame.init() # Initialsion of all pygame functions so errors do not occur.
 
-WIDTH, HEIGHT = 960, 540 # Constant values for the width and height of the widnow so that it never changes
+width, height = 2880, 1620 # The default width and height of the widnow which can be changed in the settings
+width, height = 960, 540 # The default width and height of the widnow which can be changed in the settings
 FPS = 60 # Constant value for the fps so that it never changes.
 logo = pygame.image.load("images/logo.png") # loading the logo image from the images folder
 
 class Program: # Main class from where the program will run once called upon
     def __init__(self): # Setting the attributes of the class 
-         self.window = pygame.display.set_mode((WIDTH, HEIGHT)) # Creating the main program window with the associated width and height
+         self.window = pygame.display.set_mode((width, height)) ### pygame.FULLSCREEN ### # Creating the main program window with the associated width and height and making it by default full screen.
          pygame.display.set_caption("Maze Craze") # Naming the window
          pygame.display.set_icon(logo) # Adding a custom maze image for the window
          self.clock = pygame.time.Clock() # Set a tick rate which will be used to maintain fps
          
          self.GameStateManager = GameStateManager("menuMain") # Set the default program state to menuMain
          
-         self.menuMain = Menu(self.window, self.GameStateManager, WIDTH, HEIGHT)
+         self.menuMain = Menu(self.window, self.GameStateManager, width, height)
          
     def run(self): # Called to run the program
         while True: # The game loop, once ended stops the program and keeps checking for state changes
@@ -27,12 +28,15 @@ class Program: # Main class from where the program will run once called upon
                 if event.type == pygame.QUIT: # If the window quit button is pressed pygame and the program are terminated.
                     pygame.quit()
                     exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if current_state == "menuMain":
+                        self.menuMain.click(event.pos)
                     
             if current_state == "menuMain":
                 self.menuMain.run()
                 
             pygame.display.update()
-            self.clock.tick(FPS)
+            self.clock.tick(FPS) # Setting the tick rate to the amount of frames per second (60) for the program to check for changes
     
 if __name__ == "__main__":
     program = Program()
--- menuMain.py
+++ menuMain.py
@@ -1,7 +1,7 @@
 import pygame
 from objects.button import Button
 
-def draw_objects(window, WIDTH, HEIGHT, font):
+def draw_objects(window, width, height, font):
     # Initiating all properties of each button for the main menu
     play = Button(font, "Play", (112, 137, 156), "white", 20, 30, 400, 80, "white", (180, 103, 120))
     settings = Button(font, "Settings", (112, 137, 156), "white",  20, 150, 400, 80, "white", (180, 103, 120))
@@ -14,21 +14,21 @@ def draw_objects(window, WIDTH, HEIGHT, font):
     return play, settings, quit
 
 class Menu:
-    def __init__(self, window, GameStateManager, WIDTH, HEIGHT):
+    def __init__(self, window, GameStateManager, width, height):
         self.window = window
         self.GameStateManager = GameStateManager
-        self.width = WIDTH
-        self.height = HEIGHT
+        self.width = width
+        self.height = height
         
     def run(self):
-        font = pygame.font.SysFont("Calibri", 70) # Font for text and buttons
+        font = pygame.font.SysFont("Calibri", 60, True) # Font for text and buttons
         
         self.window.fill((23, 27, 32)) # Background colour of the program
         self.play_button, self.settings_button, self.quit_button = draw_objects(self.window, self.width, self.height, font)
         
         pos = pygame.mouse.get_pos()
 
-        # Update hover effect for buttons
+        # Check if a button is hovered over and change the background colour of the button 
         self.play_button.button_hover(pos)
         self.settings_button.button_hover(pos)
         self.quit_button.button_hover(pos)
@@ -37,3 +37,14 @@ class Menu:
         self.play_button.draw_button(self.window)
         self.settings_button.draw_button(self.window)
         self.quit_button.draw_button(self.window)
+        
+    def click(self, pos):
+        if self.quit_button.rect.collidepoint(pos):
+            pygame.quit()
+            exit()
+        elif self.settings_button.rect.collidepoint(pos):
+            self.GameStateManager.set_state("menuSettings")
+            print("Entering Settings Menu")
+        elif self.play_button.rect.collidepoint(pos):
+            self.GameStateManager.set_state("menuPlay")
+            print("Entering Play Menu")
